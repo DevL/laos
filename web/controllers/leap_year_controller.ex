1fbfdf07f1308850aad016315d45f48ffa8d5780
Lennart Fridén <lennart@devl.se>
2015-02-22 10:09:06 +0100

    Eliminate use of try...raise with regex!

--- web/controllers/leap_year_controller.ex
+++ web/controllers/leap_year_controller.ex
@@ -8,11 +8,13 @@ defmodule Yearly.LeapYearController do
   end
 
   def show(conn, %{"year" => year}) do
-    try do
-      leap_year = year |> String.to_integer |> LeapYear.leap_year?
-      render conn, :show, year: year, leap_year: leap_year
-    rescue
-      _ in ArgumentError -> render conn, :bad_year, year: year
+    if valid_year? year do
+      render conn, :show, year: year, leap_year: leap_year? year
+    else
+      conn |> put_status(400) |> render :bad_year, year: year
     end
   end
+
+  defp valid_year?(year), do: year =~ ~r/\A\d+\z/
+  defp leap_year?(year), do: year |> String.to_integer |> LeapYear.leap_year?
 end
